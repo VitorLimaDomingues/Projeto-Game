98fc642ba4ce0e8659e59f33d8aca8c2e71b93e6
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2025-04-07 11:17:12 -0300

    Criando a estilização do header e footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { FooterContainer } from '../styles/FooterStyled'
 
 const Footer = () => {
   return (
-    <div>
-      <h5>@2025</h5>
+  <FooterContainer>
+
+
+    <div className="contato">
+      <h3>Contato</h3>
+      <p>Email: contato@gmail.com</p>
+      <p>Telefone: (11) 93295-4854</p>
     </div>
+
+    <div classNme="direitos">
+      <p>&copy; 2025 - City Game - Todos os direitos reservados</p>
+    </div>
+
+    <div className="redes-sociais">
+      <h3>Redes Sociais</h3>
+      <ul>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Instagram</a></li>
+        <li><a href="#">Twitter</a></li>
+      </ul>
+    </div>
+  </FooterContainer>
   )
 }
 
